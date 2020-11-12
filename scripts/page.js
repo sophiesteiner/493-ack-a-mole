42b6464fd4fea4a26b74e471be5cb4764ed55f4b
taylor <taylor@thunkable.com>
2020-11-11 19:01:51 -0500

    add sound

--- scripts/page.js
+++ scripts/page.js
@@ -2,8 +2,22 @@ $(function(){
   setTimeout(function(){
     $('#splash_screen').fadeOut();
   }, 3000);
-}) 
 
+  var audio = new Audio("images/Funny-background-music-for-games.mp3");
+
+  $("#sound_toggle").click(function() {
+    console.log("ON CLICK")
+    if (audio.paused) {
+      audio.play();
+      $("#sound_toggle img").attr("src", "images/vol_on.png");
+    } else {
+      audio.pause();
+      $("#sound_toggle img").attr("src", "images/vol_off.png");
+    }
+  });
+
+
+}) 
 /*
 Vue.component('mole', {
     data: function () {
