68700cee4c5ee22143b875ae7f28f2a24cfab2de
Meredith Allen <merallen@umich.edu>
2020-11-11 20:25:29 -0500

    yeah we are done for the night

--- scripts/page.js
+++ scripts/page.js
@@ -1,7 +1,15 @@
+// GLOBALS
+let current_difficulty = "easy";
+let good_mole_fname = "images/good_mole.png";
+let bad_mole_fname = "images/bad_mole.png";
+
+
+// MAIN
 $(function(){
   setTimeout(function(){
     $('#splash_screen').fadeOut();
   }, 3000);
+
 
   
   var audio = new Audio("images/Funny-background-music-for-games.mp3");
@@ -15,12 +23,16 @@ $(function(){
       audio.pause();
       $("#sound_toggle img").attr("src", "images/vol_off.png");
     }
+
+
   });
 
   
 
 
 }) 
+
+// SETTINGS TIME
 
 function settings_open(){
   if($('.settings_panel').css("display") === "none"){
@@ -29,8 +41,28 @@ function settings_open(){
   else{
     $('.settings_panel').css("display", "none");
   }
-  
+}
 
+function set_easy(){
+  current_difficulty = "easy";
+}
+
+function set_medium(){
+  current_difficulty = "medium";
+}
+
+function set_hard(){
+  current_difficulty = "hard";
+}
+
+function change_good_image(){
+  // TODO
+  good_mole_fname = "new";
+}
+
+function change_bad_image(){
+  // TODO
+  good_bad_fname = "new";
 }
 /*
 Vue.component('mole', {
